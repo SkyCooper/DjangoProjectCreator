cd9f39eb45ee09a7f1615643b4a9b6f2d6dc6476
Can Berk Ocalir <can.ocalir@gmail.com>
2022-12-20 13:23:08 +0300

    added several properties, views, core, settings...

--- django-project-creator.sh
+++ django-project-creator.sh
@@ -27,6 +27,14 @@ eitalic=$(tput ritm || tput ZR) # End   italic
 stout=$(tput smso || tput so)   # Start stand-out
 estout=$(tput rmso || tput se)  # End stand-out
 
+#color response creators
+function colorprint() {
+    heading="$1\t"
+    body="$2"
+    printf '%b' "${heading}"
+    printf '%b\n' "${body}${nc}" | do_the_wrap
+}
+
 function ring_the_bell() {
     [[ $bell == "true" ]] && tput bel
 }
@@ -72,14 +80,6 @@ function echo_query() {
     echo -en "${blue}${bold}${options}>${nc} "
 }
 
-function colorprint() {
-    heading="$1\t"
-    body="$2"
-    printf '%b' "${heading}"
-    printf '%b\n' "${body}${nc}" | do_the_wrap
-
-}
-
 # Wraps indents the text according to variables / terminal output
 function do_the_wrap() {
     if [[ $fold = true ]]; then
@@ -116,6 +116,7 @@ then
     if ! command -v pip &> /dev/null
     then
         echo "pip or pip3 could not be found"
+        sleep 2
         exit 1
     fi
 else
@@ -126,11 +127,13 @@ fi
 
 echo_progress_start "Starting Django project creator..."
 
+#Create Project Folder
 echo_info "Created project folder: $project"
 mkdir -p "$project"
 
 cd "$project"
 
+#Checking virtualenv installation
 echo_progress_start "Checking virtualenv installation..."
 if $pip show virtualenv >> $logfile; then
     echo_progress_done "virtualenv is already installed."
@@ -140,34 +143,42 @@ else
         echo_progress_done "Installed virtualenv."
     else
         echo_error "Installing virtualenv failed."
+        sleep 2
         exit 1
     fi
 fi
 
+#Creating virtualenv installation
 echo_progress_start "Creating virtualenv..."
 if $python -m virtualenv .env >> $logfile; then
     echo_progress_done "Created virtualenv."
 else
     echo_error "Creating virtualenv failed."
+    sleep 2
     exit 1
 fi
 
+#Activating virtualenv installation
 echo_progress_start "Activating virtualenv..."
 if source .env/bin/activate >> $logfile; then
     echo_progress_done "Activated virtualenv."
 else
     echo_error "Activating virtualenv failed."
+    sleep 2
     exit 1
 fi
 
+#Django installation
 echo_progress_start "Installing Django..."
 if $pip install django >> $logfile; then
     echo_progress_done "Installed Django."
 else
     echo_error "Installing Django failed."
+    sleep 2
     exit 1
 fi
 
+#Creating requirements.txt
 echo_progress_start "Creating requirements.txt..."
 if $pip freeze > "requirements.txt"; then
     echo_progress_done "Created requirements.txt."
@@ -175,48 +186,132 @@ if $pip freeze > "requirements.txt"; then
     cat "requirements.txt"
 else
     echo_error "Creating requirements.txt failed."
+    sleep 2
     exit 1
 fi
 
+#Creating Django project
 echo_progress_start "Creating Django project 'main'..."
 if django-admin startproject main . >> $logfile; then
     echo_progress_done "Created Django project."
 else
     echo_error "Creating Django project failed."
+    sleep 2
     exit 1
 fi
 
+#Creating Django Core Folder
+echo_progress_start "Creating the app core"
+if $python manage.py startapp core; then
+    echo_progress_done "Created app core succesfully."
+else
+    echo_error "Creating app core failed"
+    sleep 2
+    exit 1
+fi    
+
+#Creating Django Forms file
+echo_progress_start "Creating forms.py"
+if touch core/forms.py; then
+    echo_progress_done "Created forms.py successfully"
+else
+    echo_error "Creating forms.py failed"
+    sleep 2
+    exit 1
+fi
+
+#Creating Template Directory
+echo_progress_start "Creating template directory"
+if mkdir core/templates; then
+    echo_progress_done "Created template directory successfully"
+else
+    echo_error "Creating template directory failed"
+    sleep 2
+    exit 1
+fi        
+
+#Creating Example Template
+echo_progress_start "Creating sample index.html"
+if cat << EOF > core/templates/index.html
+<html>
+  <head>
+   <title>$project</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Welcome to your Create Django App</h1>
+    </div>
+  </body>
+</html>
+EOF
+then
+    echo_progress_done "Created sample index.html successfully"
+else
+    echo_error "Creating index.html failed"
+    sleep 2
+    exit 1
+fi    
+
+#Adding created app to the settings
+echo_progress_start "Editing settings.py"
+if sed -i "/django.contrib.staticfiles/a\    'main'," main/settings.py; then
+    echo_progress_done "Editing settings.py successfully"    
+else
+    echo_error "Editing settings file failed"
+    sleep 2
+    exit 1
+fi
+
+#Creating a Simple View
+echo_progress_start "Create a simple view"
+if sed -i "2d" core/views.py && sed -i "2c\from django.http import HttpResponse\n\n\n#def home(request):\n    #return HttpResponse('<h1>Welcome to the Django.</h1>')\n\n" core/views.py
+    echo_progress_done "Created a simple view successfully"
+then
+    echo "def home(request):" >> core/views.py
+    echo "    return render(request, 'index.html')" >> core/views.py
+else
+    echo_error "View creation failed"
+    sleep 2
+    exit 1
+fi
+
+#Complete Django Migration
 echo_progress_start "Starting Django migration..."
 if $python manage.py migrate >> $logfile; then
     echo_progress_done "Django migration is successful."
 else
     echo_error "Django migration failed."
+    sleep 2
     exit 1
 fi
 
+#Creating Django Superuser
 echo_progress_start "Creating superuser..."
 if $python manage.py createsuperuser; then
     echo_progress_done "Created superuser."
 else
     echo_error "superuser creation failed."
+    sleep 2
     exit 1
 fi
 
+#Adding .gitignore file
 echo_progress_start "Downloading .gitignore for Django project..."
 if curl "https://www.toptal.com/developers/gitignore/api/django" --output ".gitignore" >> $logfile 2>&1; then
     echo_progress_done "Downloaded .gitignore successfully."
 else
     echo_error "Downloading .gitignore failed. Please make sure curl is installed."
+    sleep 2
     exit 1
 fi
 
+sleep 2
+echo "${green}>>> Done${nc}"
+echo_progress_done "Django Project created successfully"
+sleep 2
+
+#Starting the Project
+echo_progress_start "Starting the Project Now"
+python manage.py runserver $port
+echo_progress_done "Project started successfully"
 deactivate
-
-echo_success "Created Django project."
-echo ""
-echo_info "Use following commands to start your project:"
-echo ""
-echo_info "cd $project"
-echo_info "source .env/bin/activate"
-
-echo_info "$python manage.py runserver $port"
