f1d01a0bb8464c2e3cb3bcb5ec4b0f744a6b408d
Ulna <30731799+ulnasensei@users.noreply.github.com>
2022-12-21 14:20:40 +0300

    add exit handler, sample views, runserver

--- django-project-creator.py
+++ django-project-creator.py
@@ -1,10 +1,12 @@
 from virtualenv import cli_run
 import urllib.request
-from subprocess import check_output, run, DEVNULL
+from subprocess import check_call, run, DEVNULL
 import sys
 import os
 import logging
 import psutil
+import signal
+
 
 class bcolors:
     HEADER = "\033[95m"
@@ -18,10 +20,15 @@ class bcolors:
     UNDERLINE = "\033[4m"
 
 
+def exit_handler(signum, frame):
+    exit(1)
+
+
 def main():
+    signal.signal(signal.SIGINT, exit_handler)
     windows = True if os.name == "nt" else False
     git_bash = False
-    if windows and  psutil.Process(os.getppid()).name() == "bash.exe":
+    if windows and psutil.Process(os.getppid()).name() == "bash.exe":
         git_bash = True
 
     logfile = f'{os.path.expanduser("~")}/django-project-creator.log'
@@ -102,9 +109,19 @@ def main():
     try:
         print(f"{bcolors.OKCYAN}Installing Django...{bcolors.ENDC}")
         if windows:
-            run(f".\\.venv\\Scripts\\python -m pip install django", shell=True, stdout=DEVNULL, check=True)
+            run(
+                f".\\.venv\\Scripts\\python -m pip install django",
+                shell=True,
+                stdout=DEVNULL,
+                check=True,
+            )
         else:
-            run(f"./.venv/bin/python -m pip install django", shell=True, stdout=DEVNULL, check=True)
+            run(
+                f"./.venv/bin/python -m pip install django",
+                shell=True,
+                stdout=DEVNULL,
+                check=True,
+            )
         print(f"{bcolors.OKGREEN}Installed Django.{bcolors.ENDC}")
         logger.info("Installed Django.")
     except Exception as ex:
@@ -115,9 +132,17 @@ def main():
     try:
         print(f"{bcolors.OKCYAN}Creating requirements.txt...{bcolors.ENDC}")
         if windows:
-            run(".\\.venv\\Scripts\\python -m pip freeze > requirements.txt", shell=True, check=True)
+            run(
+                ".\\.venv\\Scripts\\python -m pip freeze > requirements.txt",
+                shell=True,
+                check=True,
+            )
         else:
-            run("./.venv/bin/python -m pip freeze > requirements.txt", shell=True, check=True)
+            run(
+                "./.venv/bin/python -m pip freeze > requirements.txt",
+                shell=True,
+                check=True,
+            )
         print(f"{bcolors.OKGREEN}Created requirements.txt.{bcolors.ENDC}")
         logger.info("Created requirements.txt.")
         with open("requirements.txt", "r") as reqs:
@@ -130,14 +155,123 @@ def main():
         logger.fatal(ex)
         exit(1)
     try:
-        print(f"{bcolors.OKCYAN}Creating Django project 'main'...{bcolors.ENDC}")
-        
+        print(f"{bcolors.OKCYAN}Creating Django project 'core'...{bcolors.ENDC}")
+
         if windows:
-            run(".\\.venv\\Scripts\\python -m django startproject main .", shell=True, check=True)
+            run(
+                ".\\.venv\\Scripts\\python -m django startproject core .",
+                shell=True,
+                check=True,
+            )
         else:
-            run("django-admin startproject main .", shell=True, check=True)
-        print(f"{bcolors.OKGREEN}Created Django project.{bcolors.ENDC}")
-        logger.info("Created Django project.")
+            run("django-admin startproject core .", shell=True, check=True)
+        print(f"{bcolors.OKGREEN}Created Django project 'core'.{bcolors.ENDC}")
+        logger.info("Created Django project 'core'.")
+    except Exception as ex:
+        print(f"{bcolors.BOLD}{bcolors.FAIL}{ex}{bcolors.ENDC}")
+        logger.fatal(ex)
+        exit(1)
+
+    try:
+        print(f"{bcolors.OKCYAN}Creating Django app 'main'...{bcolors.ENDC}")
+
+        if windows:
+            run(
+                ".\\.venv\\Scripts\\python -m django startapp main",
+                shell=True,
+                check=True,
+            )
+        else:
+            run("django-admin startapp main", shell=True, check=True)
+        print(f"{bcolors.OKGREEN}Created Django app 'main'.{bcolors.ENDC}")
+        logger.info("Created Django app 'main'.")
+    except Exception as ex:
+        print(f"{bcolors.BOLD}{bcolors.FAIL}{ex}{bcolors.ENDC}")
+        logger.fatal(ex)
+        exit(1)
+
+    try:
+        print(f"{bcolors.OKCYAN}Creating 'forms.py'...{bcolors.ENDC}")
+
+        with open("./main/forms.py", "w") as f:
+            pass
+        print(f"{bcolors.OKGREEN}Created 'forms.py'.{bcolors.ENDC}")
+        logger.info("Created 'forms.py'.")
+    except Exception as ex:
+        print(f"{bcolors.BOLD}{bcolors.FAIL}{ex}{bcolors.ENDC}")
+        logger.fatal(ex)
+        exit(1)
+
+    try:
+        print(f"{bcolors.OKCYAN}Creating template directory...{bcolors.ENDC}")
+
+        os.mkdir("./main/templates")
+        print(f"{bcolors.OKGREEN}Created template directory.{bcolors.ENDC}")
+        logger.info("Created template directory.")
+    except Exception as ex:
+        print(f"{bcolors.BOLD}{bcolors.FAIL}{ex}{bcolors.ENDC}")
+        logger.fatal(ex)
+        exit(1)
+
+    try:
+        print(f"{bcolors.OKCYAN}Creating sample index.html...{bcolors.ENDC}")
+
+        index_html = """<html>
+  <head>
+   <title>$project</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Welcome to your Create Django App</h1>
+    </div>
+  </body>
+</html>"""
+        with open("main/templates/index.html", "w") as f:
+            f.write(index_html)
+
+        print(f"{bcolors.OKGREEN}Created sample index.html.{bcolors.ENDC}")
+        logger.info("Created sample index.html.")
+    except Exception as ex:
+        print(f"{bcolors.BOLD}{bcolors.FAIL}{ex}{bcolors.ENDC}")
+        logger.fatal(ex)
+        exit(1)
+
+    try:
+        print(f"{bcolors.OKCYAN}Updating settings.py...{bcolors.ENDC}")
+        with open("./core/settings.py", "r") as file:
+            filedata = file.read()
+
+        filedata = filedata.replace(
+            '"django.contrib.staticfiles",',
+            """"django.contrib.staticfiles",\n    "main",""",
+        )
+
+        with open("./core/settings.py", "w") as file:
+            file.write(filedata)
+
+        print(f"{bcolors.OKCYAN}Updated settings.py{bcolors.ENDC}")
+    except Exception as ex:
+        print(f"{bcolors.BOLD}{bcolors.FAIL}{ex}{bcolors.ENDC}")
+        logger.fatal(ex)
+        exit(1)
+
+    try:
+        print(f"{bcolors.OKCYAN}Creating sample view...{bcolors.ENDC}")
+        with open("./main/views.py", "w") as file:
+            file.write(
+                """from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# def home(request):
+#     return HttpResponse("<h1>Welcome to the Django.</h1>")
+
+
+def home(request):
+    return render(request, "index.html")"""
+            )
+
+        print(f"{bcolors.OKCYAN}Created sample view...{bcolors.ENDC}")
     except Exception as ex:
         print(f"{bcolors.BOLD}{bcolors.FAIL}{ex}{bcolors.ENDC}")
         logger.fatal(ex)
@@ -159,7 +293,11 @@ def main():
     try:
         print(f"{bcolors.OKCYAN}Creating superuser...{bcolors.ENDC}")
         if windows:
-            run(".\\.venv\\Scripts\\python manage.py createsuperuser", shell=True, check=True)
+            run(
+                ".\\.venv\\Scripts\\python manage.py createsuperuser",
+                shell=True,
+                check=True,
+            )
         else:
             run("./.venv/bin/python manage.py createsuperuser", shell=True, check=True)
         print(f"{bcolors.OKGREEN}Created superuser.{bcolors.ENDC}")
@@ -195,21 +333,40 @@ def main():
         logger.fatal(ex)
         exit(1)
     print(f"\n{bcolors.BOLD}{bcolors.OKGREEN}Created Django project.{bcolors.ENDC}\n")
-    print(
-        f"{bcolors.OKBLUE}Use following commands to start your project:{bcolors.ENDC}\n"
-    )
-    print(f"{bcolors.BOLD}{bcolors.OKCYAN}cd {project_name}{bcolors.ENDC}")
 
-    if git_bash:
-        run(".\\.venv\\Scripts\\deactivate", shell=True, check=True)
-    
-    if git_bash:
-        print(f"{bcolors.BOLD}{bcolors.OKCYAN}source .venv/Scripts/activate{bcolors.ENDC}")
-    elif windows:
-        print(f"{bcolors.BOLD}{bcolors.OKCYAN}.\\.venv\\Scripts\\activate{bcolors.ENDC}")
-    else:
-        print(f"{bcolors.BOLD}{bcolors.OKCYAN}source .venv/bin/activate{bcolors.ENDC}")
-    exit(0)
+    try:
+        print(
+            f"{bcolors.OKCYAN}Running the server on port {bcolors.BOLD}8081...{bcolors.ENDC}\n"
+        )
+        if windows:
+            check_call(
+                ".\\.venv\\Scripts\\python manage.py runserver 8081",
+                shell=True,
+            )
+        else:
+            check_call("./.venv/bin/python manage.py runserver 8081", shell=True)
+        logger.info("Running the server on port 8081")
+    except Exception as ex:
+        print(f"{bcolors.BOLD}{bcolors.FAIL}{ex}{bcolors.ENDC}")
+        logger.fatal(ex)
+        exit(1)
+
+    # print(f"{bcolors.BOLD}{bcolors.OKCYAN}cd {project_name}{bcolors.ENDC}")
+
+    # if git_bash:
+    #     run(".\\.venv\\Scripts\\deactivate", shell=True, check=True)
+
+    # if git_bash:
+    #     print(
+    #         f"{bcolors.BOLD}{bcolors.OKCYAN}source .venv/Scripts/activate{bcolors.ENDC}"
+    #     )
+    # elif windows:
+    #     print(
+    #         f"{bcolors.BOLD}{bcolors.OKCYAN}.\\.venv\\Scripts\\activate{bcolors.ENDC}"
+    #     )
+    # else:
+    #     print(f"{bcolors.BOLD}{bcolors.OKCYAN}source .venv/bin/activate{bcolors.ENDC}")
+    # exit(0)
 
 
 if __name__ == "__main__":
